f4639b9f17d129b1c559bb4a6e94444180a16bc1
EL Felaouss mohamed elamine <elfelaoussmohamedelamine@gmail.com>
2025-03-17 21:28:51 +0000

    feat(core): create Express server with MongoDB connection and middleware setup

---
+++ social-media-app-back-end/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// importing DB CONNECTION FROM CONFIG FILE
+const { connectDB } = require("./src/config/db");
+// connecting to db
+connectDB();
+
+// increase request body size limit
+app.use(express.json({ limit: "50mb" }));
+
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("welcome to social app API");
+});
+
+app.listen(PORT, () => console.log(`server is running under port ${PORT}`));
