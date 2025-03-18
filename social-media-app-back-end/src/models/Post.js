7206d8365fe4172c96fde05759db3148783ebbf5
EL Felaouss mohamed elamine <elfelaoussmohamedelamine@gmail.com>
2025-03-18 16:11:32 +0000

    feat(models): add comments field to Post schema

--- social-media-app-back-end/src/models/Post.js
+++ social-media-app-back-end/src/models/Post.js
@@ -10,6 +10,7 @@ const PostSchema = new mongoose.Schema(
       required: true,
     },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   },
   { timestamps: true }
 );
